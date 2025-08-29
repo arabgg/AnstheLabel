8c5571fd28768e1acb19c975655239c3ba2dff5c
= <=>
2025-08-29 17:58:25 +0700

    penggabungan "laravel-mix": "^6.0.49" dan "tailwindcss": "^4.1.12"

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,13 @@
 const mix = require('laravel-mix');
+const postcssImport = require('postcss-import');
+
+mix.postCss('resources/css/app.css', 'public/css', [
+    postcssImport,
+    require('autoprefixer'),
+]);
 
 mix.css('resources/css/style.css', 'public/css');
+
+if (mix.inProduction()) {
+    mix.version();
+}
