caf81e97e382cd138e258d84817377a71cdf36bc
Ahmad Mimna'i Fiddin <ahmadfiddin687@gmail.com>
2025-11-07 13:00:06 +0700

    add: DomPdf to aliases

--- config/app.php
+++ config/app.php
@@ -184,6 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Pdf' => Barryvdh\DomPDF\Facade\Pdf::class,
         // 'Example' => App\Facades\Example::class,
     ])->toArray(),
 
